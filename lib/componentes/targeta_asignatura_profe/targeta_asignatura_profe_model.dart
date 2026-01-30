dd749cc5cc438d0b8d9e79c1d4f9fed58c6bef56
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-01-30 18:43:40 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/componentes/targeta_asignatura_profe/targeta_asignatura_profe_model.dart
@@ -0,0 +1,12 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'targeta_asignatura_profe_widget.dart' show TargetaAsignaturaProfeWidget;
+import 'package:flutter/material.dart';
+
+class TargetaAsignaturaProfeModel
+    extends FlutterFlowModel<TargetaAsignaturaProfeWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
